92873c72b4af5ec44d31f02cea0c14b9fd6695e9
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2021-10-28 08:54:56 -0300

    cambios productList y estructura

--- CartApp.js
+++ CartApp.js
@@ -1,6 +1,29 @@
+const listaProductos = document.querySelector('#lista-productos');
 const tableCarrito = document.querySelector('#lista-carrito tbody');
+const formBuscador = document.querySelector('#formulario');
+const btnVaciarCarrito = document.querySelector('#vaciar-carrito');
 
+let carrito;
+
+document.addEventListener("DOMContentLoaded", () => {
+	productList(productos);
+});
+
+listaProductos.addEventListener('click', agregarProducto);
+formBuscador.addEventListener('submit', buscarProductos);
 tableCarrito.addEventListener('click', eliminarProducto);
+btnVaciarCarrito.addEventListener('click', vaciarCarrito);
+
+function vaciarCarrito(e) {
+	e.preventDefault();
+	// Vaciar el arreglo carrito;
+	carrito = [];
+
+	// Actualizar HTML del carrito
+	actualizarCarritoHTML();
+	// Actualizar el storage del carrito
+	actualizarStorage();
+}
 
 function eliminarProducto(e) {
 	e.preventDefault();
@@ -20,14 +43,89 @@ function eliminarProducto(e) {
 	}
 }
 
+function agregarProducto(e) {
+	e.preventDefault();
+
+	if (e.target.classList.contains("agregar-carrito")) {
+		const productCard = e.target.parentElement.parentElement;
+
+		const productoAgregado = {
+			imagen: productCard.querySelector('img.imagen-producto').src,
+			nombre: productCard.querySelector('h4').textContent,
+			precio: productCard.querySelector('.precio span').textContent,
+			cantidad: 1,
+			id: productCard.querySelector('a').dataset.id
+		}
+
+		// Chequear si productoAgregado existe en el carrito
+
+		const existe = carrito.some(producto => producto.id === productoAgregado.id);
+		// const index = carrito.findIndex( producto => producto.id === productoAgregado.id );
+
+		//if(index !== -1) {
+		// carrito[index].cantidad++
+		//}
+		if (existe) {
+			const nuevoCarrito = carrito.map(producto => {
+				if (producto.id === productoAgregado.id) {
+					producto.cantidad++;
+				}
+				return producto;
+			});
+			carrito = [...nuevoCarrito];
+			// carrito = nuevoCarrito;
+		} else {
+			// Se agrega por primera vez
+			carrito.push(productoAgregado);
+			// carrito = [...carrito, productoAgregado] // = push
+		}
+
+		// Renderizo la tabla con los items del carrito
+		actualizarCarritoHTML();
+		actualizarStorage();
+	}
+}
+
+function actualizarCarritoHTML() {
+	tableCarrito.innerHTML = '';
+
+	carrito.forEach(producto => {
+		const { imagen, nombre, precio, cantidad, id } = producto;
+
+		tableCarrito.innerHTML += `
+				<tr>
+					<td>
+						<img src="${imagen}" width="100%">
+					</td>
+					<td>
+						${nombre}
+					</td>
+					<td>
+						${precio}
+					</td>
+					<td>
+						${cantidad}
+					</td>
+					<td>
+						<a href="#" class="borrar-producto" data-id="${id}"><i class="fas fa-trash"></i></a>
+					</td>
+				</tr>
+			`;
+	});
+}
+
+function actualizarStorage() {
+	// TODO
+	localStorage.setItem('carrito', JSON.stringify(carrito));
+}
+
 /* carga/genera dinamicamente Cards */
-document.addEventListener("DOMContentLoaded", () => {
-	const list = document.querySelector('#product-list');
+function productList(productos) {
 
 	productos.forEach(product => {
 		const col = document.createElement('div');
 		col.className = "col";
-		list.appendChild(col);
+		listaProductos.appendChild(col);
 
 		const card = document.createElement('div');
 		card.className = "card";
@@ -58,24 +156,26 @@ document.addEventListener("DOMContentLoaded", () => {
 		cardButton.setAttribute('data-bs-toggle', "modal");
 		cardButton.setAttribute('data-bs-target', "#exampleModal");
 		card.appendChild(cardButton);
-		// Modal
-		list.innerHTML += `
-			<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-			<div class="modal-dialog">
-				<div class="modal-content">
-					<div class="modal-header">
-						<h5 class="modal-title" id="exampleModalLabel">Felicitaciones!</h5>
-						<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-					</div>
-					<div class="modal-body">
-						Has agregado el producto al carrito
-					</div>
-					<div class="modal-footer">
-						<button type="button" class="btn btn-primary" data-bs-dismiss="modal">Ok</button>
+
+		const modal = `
+				<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+					<div class="modal-dialog">
+						<div class="modal-content">
+							<div class="modal-header">
+								<h5 class="modal-title" id="exampleModalLabel">Felicitaciones!</h5>
+								<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+							</div>
+							<div class="modal-body">
+								Has agregado el producto al carrito
+							</div>
+							<div class="modal-footer">
+								<button type="button" class="btn btn-primary" data-bs-dismiss="modal">Ok</button>
+							</div>
+						</div>
 					</div>
 				</div>
-			</div>
-		</div>
 		`;
+
+		listaProductos.innerHTML += modal;
 	})
-});
+}
